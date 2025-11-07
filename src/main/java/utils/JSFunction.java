71add3630c9d67f717f796131d3931b806e33c19
leeyc2014 <leeyc20145@naver.com>
2025-11-07 13:24:43 +0900

    14장 실습 완료

--- src/main/java/utils/JSFunction.java
+++ src/main/java/utils/JSFunction.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.jsp.JspWriter;
 
 public class JSFunction {
@@ -25,6 +28,35 @@ public class JSFunction {
 						  + "	history.back();"
 						  + "</script>";
 			out.print(script);
+		}
+		catch (Exception e) {}
+	}
+	
+	// 메시지 알림창을 띄운 후 명시한 URL로 이동합니다.
+	public static void alertLocation(HttpServletResponse resp, String msg, String url) {
+		try {
+			resp.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = resp.getWriter();
+			String script = ""	// 삽입할 자바스크립트 코드
+						  + "<script>"
+						  + "	alert('" + msg + "');"
+						  + "	location.href='" + url + "';"
+						  + "</script>";
+			writer.print(script);
+		}
+		catch (Exception e) {}		
+	}
+	
+	public static void alertBack(HttpServletResponse resp, String msg) {
+		try {
+			resp.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = resp.getWriter();
+			String script = ""
+						  + "<script>"
+						  + "	alert('" + msg + "');"
+						  + "	history.back();"
+						  + "</script>";
+			writer.print(script);
 		}
 		catch (Exception e) {}
 	}
